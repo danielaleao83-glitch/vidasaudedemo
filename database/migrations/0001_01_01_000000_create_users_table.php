9a2dd51994b67aca47eaa63e5d1834fe61e56d2c
Daniela Leao <daniela.leao83@gmail.com>
2026-09-24 17:41:39 -0300

    Corrige migration de sessions e ignora SQLite local

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -35,7 +35,6 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
 
-            $table->index('last_activity');
         });
     }
 
